be981c7c871ef585383cb1fdd30b75a3dafa919c
Juan Galvez <contacto@lifearquitectos.com>
2023-12-11 21:48:17 -0500

    changes

--- src/app/api/getProjectByName/route.js
+++ src/app/api/getProjectByName/route.js
@@ -6,14 +6,6 @@ import Project from '@/models/project';
 import mongoose from 'mongoose';
 
 export async function POST(request) {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json(
-      { message: 'You must be logged in.' },
-      { status: 402 }
-    );
-  }
   try {
     await connectDB();
     const { titulo } = await request.json();
